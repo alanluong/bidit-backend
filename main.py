ebfa69ed5db5a7365765571fa4ff4e1af81fcdd2
Eddie An <ehdee@Eddies-MacBook-Pro.local>
2014-02-10 12:48:29 -0800

    mapping to existing database

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from config import db, app
+from models import *
 
 @app.route("/login")
 def login():
@@ -6,11 +7,20 @@ def login():
 
 @app.route('/create_testdb')
 def create_testdb():
-	db.create_all()
-	test = Ad('test@testing.com', 23, 'this is a test ad')
+	test = Ad('test@testing.com', 50, 'testingggg' )
 	db.session.add(test)
 	db.session.commit()
 	return 'created tables'
+
+@app.route('/create_testdb_del')
+def del_test():
+	test = Ad.query.filter_by(uploader_email='test2@testing.com').first()
+	print test.price
+	db.session.delete(test)
+	db.session.commit()
+	return 'delete entry'
+
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- models.py
+++ models.py
@@ -1,6 +1,20 @@
 from config import db
 
+
+
+class User(db.Model):
+	__tablename__ = 'User'
+	email = db.Column(db.String(50), primary_key=True)
+	first_name = db.Column(db.String(45))
+	last_name = db.Column(db.String(45))
+
+	def __init__(self, email, fn, ln):
+		self.email = email
+		self.first_name = fn
+		self.last_name = ln
+
 class Ad(db.Model):
+	__tablename__ = 'Ad'
 	uploader_email = db.Column(db.String(50), primary_key=True)
 	price = db.Column(db.Integer)
 	description = db.Column(db.String(45))
