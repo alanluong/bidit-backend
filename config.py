0cf913f37ae0ea69910dbb96503d7e170c624359
Alan <aluong91@gmail.com>
2014-03-05 10:55:03 -0800

    login form

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.restless import APIManager
+from flask.ext.login import LoginManager
 
 PORT = 5000
 app = Flask(__name__)
@@ -8,3 +9,4 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://bidit:bidit@ec2-54-213-102-70.us-west-2.compute.amazonaws.com/bidit_test'
 db = SQLAlchemy(app)
 manager = APIManager(app, flask_sqlalchemy_db = db)
+login = LoginManager(app)
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from config import db, app, manager, PORT
+from config import db, app, manager, PORT, login
 from models import *
-from flask import jsonify
-import test
 import os
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, jsonify
 from werkzeug.utils import secure_filename
 from flask import send_from_directory
+from flask.ext.login import current_user
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_FOLDER = os.path.join(APP_ROOT, 'upload')
@@ -21,51 +20,54 @@ manager.create_api(Bid, methods=['GET', 'POST'])
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+	def allowed_file(filename):
+		return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route('/cell_upload', methods=['GET', 'POST'])
 def upload_file():
-    if request.method == 'POST':
-
-	ad = Ad(request.form['email'], request.form['price'], request.form['description'])
-	db.session.add(ad)
-	db.session.commit()
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(str(ad.id)+".jpg")
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return jsonify(path= os.path.join(app.config['UPLOAD_FOLDER'], filename))
+	if request.method == 'POST':
+		ad = Ad(request.form['email'], request.form['price'], request.form['description'])
+		db.session.add(ad)
+		db.session.commit()
+		file = request.files['file']
+		if file and allowed_file(file.filename):
+			filename = secure_filename(str(ad.id)+".jpg")
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			return jsonify(path= os.path.join(app.config['UPLOAD_FOLDER'], filename))
 	"""
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			return redirect(url_for('uploaded_file',
+									filename=filename))
 	"""
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
+	return '''
+	<!doctype html>
+	<title>Upload new File</title>
+	<h1>Upload new File</h1>
+	<form action="" method=post enctype=multipart/form-data>
+	  <p><input type=file name=file>
 	 <input type=number name=price>
 	 <input type=text name=description>
 	 <input type=email name=email>
-         <input type=submit value=Upload>
-    </form>
-    '''
+		 <input type=submit value=Upload>
+	</form>
+	'''
 
 @app.route("/")
 def hello():
-    return "Hello World!!!"
+	return "Hello World!!!"
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+	if current_user.is_authenticated():
+		return 'already logged in'
+	if request.method == 'POST':
+		user = request.form['user']
+		password = request.form['password']
+		return user + password
 	return 'enter a login'
 
 @app.route('/create_testdb')
